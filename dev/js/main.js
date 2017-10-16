9ce3ec4a8ba215b35365049af22ad87271f24083
willcodes <difruscio.dev@gmail.com>
2017-10-15 20:56:26 -0400

    added js to remove sidebar

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,7 +1,20 @@
-'use strict';
+"use strict";
 
-console.log('hello, worldddd');
-
-
-var foo = "bar";
-
+/**
+ * event listener for resizing window to remove sidebar, can be expanded out to add navigation functionality and animation.
+ */
+window.onload = function() {
+//   console.log("loaded");
+  window.addEventListener("resize", () => {
+    const nav = document.getElementsByClassName("nav")[0];
+    const main = document.getElementById("content");
+    //   console.log(window.innerWidth)
+    if (window.innerWidth < 768) {
+      nav.style.display = "none";
+      main.style.width = "100vw";
+    } else {
+      nav.style.display = "block";
+      main.style.width = "calc(100vw - 300px)"
+    }
+  });
+};
